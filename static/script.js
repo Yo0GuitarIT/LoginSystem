ead5e61af7405df3e117c0c948a5fbce3ed25ace
Chen Yu Ling <118150842+Yo0GuitarIT@users.noreply.github.com>
2023-07-30 00:24:32 +0800

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,19 @@
+const checkbox = document.getElementById("checkbox");
+const calculate = document.getElementById("calculate");
+
+let validateForm = () => {
+  if (!checkbox.checked) {
+    alert("Please check the Acception First");
+    return false; // 阻止表單提交
+  }
+  return true; // 允許表單提交
+};
+
+calculate.addEventListener("click", () => {
+  const number = document.getElementById("number").value.trim();
+  if (/^[1-9]\d*$/.test(number)) {
+    window.location.href = "/square/" + number;
+  } else {
+    alert("Please enter a positive integer");
+  }
+});
